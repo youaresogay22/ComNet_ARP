98b0fe3aebc2400733947662f9ff71a50b9b806c
전석준 <201402417>
2019-10-18 16:20:40 +0900

    TCP 와 IP layer 수정하였습니다.

--- src/ARP/TCPLayer.java
+++ src/ARP/TCPLayer.java
@@ -3,26 +3,28 @@ package ARP;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-
 public class TCPLayer implements BaseLayer {
 	public int nUpperLayerCount = 0;
 	public String pLayerName = null;
 	public BaseLayer p_UnderLayer = null;
 	public ArrayList<BaseLayer> p_aUpperLayer = new ArrayList<BaseLayer>();
 
-	private class _TCP_HEADER {	// 24byte
+	private class _TCP_HEADER { // 24byte
 		// sport와 dport는 추가구현 때 ChatApp/FileApp을 분별하기 위한 것?
-		byte[] tcp_sport;	//source port 			(O, 2byte)
-		byte[] tcp_dport;	//destination port		(O, 2byte)
-		byte[] tcp_seq;		//sequence number		(X, 4byte)
-		byte[] tcp_ack;		//acknowledged sequence	(X, 4byte)
-		byte tcp_offset;	//fragment -> no use	(X, 1byte)
-		byte tcp_flag;		//control flag			(X, 1byte)
-		byte[] tcp_window;	//no use				(X, 2byte)
-		byte[] tcp_cksum;	//check sum				(X, 2byte)
-		byte[] tcp_urgptr;	//no use				(X, 2byte)
-		byte[] Padding;		//no use				(X, 4byte)
-		byte[] tcp_data;	//						(O, TCP_DATA_SIZE)
+		// sport는 source port address로 자신의 ephemeral port number를,
+		// dport는 destination port address로 받는 상대의 well-known port number를 씁니다
+		// 추가 구현 시 setSportnum(), setDportnum() 같은 함수를 사용해야겠죠
+		byte[] tcp_sport; // source port (O, 2byte)
+		byte[] tcp_dport; // destination port (O, 2byte)
+		byte[] tcp_seq; // sequence number (X, 4byte)
+		byte[] tcp_ack; // acknowledged sequence (X, 4byte)
+		byte tcp_offset; // fragment -> no use (X, 1byte)
+		byte tcp_flag; // control flag (X, 1byte)
+		byte[] tcp_window; // no use (X, 2byte)
+		byte[] tcp_cksum; // check sum (X, 2byte)
+		byte[] tcp_urgptr; // no use (X, 2byte)
+		byte[] padding; // no use (X, 4byte)
+		byte[] tcp_data; // (O, TCP_DATA_SIZE)
 
 		public _TCP_HEADER() {
 			this.tcp_sport = new byte[2];
@@ -34,46 +36,65 @@ public class TCPLayer implements BaseLayer {
 			this.tcp_window = new byte[2];
 			this.tcp_cksum = new byte[2];
 			this.tcp_urgptr = new byte[2];
-			this.Padding = new byte[4];
-			this.tcp_data = null;	// new
+			this.padding = new byte[4];
+			this.tcp_data = null; // new
 		}
 	}
 
 	_TCP_HEADER m_tHeader = new _TCP_HEADER();
-	
+
 	public TCPLayer(String pName) {
 		// super(pName);
 		// TODO Auto-generated constructor stub
 		pLayerName = pName;
-		//ResetHeader();
-	}
-	
-
-	public byte[] getPortNumber() {//포트 넘버를 아래 계층에서 가져오는 메소드
-		return null;//TCP계층 내의 메소드를 제거할 방법이 없는지 확인
+		ResetHeader();
 	}
 
-	public boolean Send() {
-		byte[] buf = new byte[24];
-		
-		System.arraycopy(m_tHeader.tcp_sport, 0, buf, 0, 2);	// buf에 2byte sport 초기화
-		System.arraycopy(m_tHeader.tcp_dport, 0, buf, 2, 2);	// 2byte dport
-		System.arraycopy(m_tHeader.tcp_seq, 0, buf, 4, 4);		// 4byte sqe
-		System.arraycopy(m_tHeader.tcp_ack, 0, buf, 8, 4);		// 4byte ack
-		buf[12] = m_tHeader.tcp_offset;							// 1byte offset
-		buf[13] = m_tHeader.tcp_flag;							// 1byte flag
-		System.arraycopy(m_tHeader.tcp_window, 0, buf, 14, 2); 	// 2byte window
-		System.arraycopy(m_tHeader.tcp_cksum, 0, buf, 16, 2);	// 2byte cksum
-		System.arraycopy(m_tHeader.tcp_urgptr, 0, buf, 18, 2);	// 2byte urgptr
-		System.arraycopy(m_tHeader.Padding, 0, buf, 20, 4);		// 4byte padding
-		
-		// Debug
-		for(int i=0; i<buf.length; i++) 
-			System.out.print(buf[i]+ " ");
-		System.out.println("\nlength = "+buf.length);
-		System.out.println("TCP Send");
-		
-		this.GetUnderLayer().Send(buf, buf.length);
+	public void ResetHeader() {
+		for (int i = 0; i < 4; i++) {
+			m_tHeader.tcp_seq[i] = (byte) 0x00;
+			m_tHeader.tcp_ack[i] = (byte) 0x00;
+			m_tHeader.padding[i] = (byte) 0x00;
+		}
+		for (int i = 0; i < 2; i++) {
+			m_tHeader.tcp_sport[i] = (byte) 0x00;
+			m_tHeader.tcp_dport[i] = (byte) 0x00;
+			m_tHeader.tcp_window[i] = (byte) 0x00;
+			m_tHeader.tcp_cksum[i] = (byte) 0x00;
+			m_tHeader.tcp_urgptr[i] = (byte) 0x00;
+		}
+		m_tHeader.tcp_data = null;
+	}
+
+	//public byte[] getPortNumber() {// 포트 넘버를 아래 계층에서 가져오는 메소드
+		//return null;// TCP계층 내의 메소드를 제거할 방법이 없는지 확인
+	//} 추가 구현 메소드이므로 주석처리함
+
+	public byte[] ObjToByte(_TCP_HEADER Header, byte[] input, int length) {
+		byte[] buf = new byte[length + 24];
+
+		System.arraycopy(Header.tcp_sport, 0, buf, 0, 2); // buf에 2byte sport 초기화
+		System.arraycopy(Header.tcp_dport, 0, buf, 2, 2); // 2byte dport
+		System.arraycopy(Header.tcp_seq, 0, buf, 4, 4); // 4byte sqe
+		System.arraycopy(Header.tcp_ack, 0, buf, 8, 4); // 4byte ack
+		buf[12] = Header.tcp_offset; // 1byte offset
+		buf[13] = Header.tcp_flag; // 1byte flag
+		System.arraycopy(Header.tcp_window, 0, buf, 14, 2); // 2byte window
+		System.arraycopy(Header.tcp_cksum, 0, buf, 16, 2); // 2byte cksum
+		System.arraycopy(Header.tcp_urgptr, 0, buf, 18, 2); // 2byte urgptr
+		System.arraycopy(Header.padding, 0, buf, 20, 4); // 4byte padding
+
+		for (int i = 0; i < length; i++)
+			buf[24 + i] = input[i];
+
+		return buf;
+	}
+
+	public boolean Send(byte[] input, int length) {
+		//send 내에 구현사항을 많이 넣으면 가독성이 떨어져서 ObjToByte로 분리했습니다.
+		byte[] TCP_header_added_bytes = ObjToByte(m_tHeader, input, length);
+
+		this.GetUnderLayer().Send(TCP_header_added_bytes, TCP_header_added_bytes.length);
 		return false;
 	}
 
@@ -82,6 +103,7 @@ public class TCPLayer implements BaseLayer {
 	}
 
 	public boolean Receive(byte[] input) {
+		//기본 구현 사항에서는 TCP layer는 reecive하지 않습니다.
 		return false;
 	}
 
