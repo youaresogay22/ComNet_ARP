90f83777e93cee2b613fe25b857210d68595fdbe
림정아 <@DESKTOP-1FFNFNT>
2019-11-29 01:39:10 +0900

    RoutingTable get함수들 추가

--- src/ARP/RoutingTable.java
+++ src/ARP/RoutingTable.java
@@ -2,16 +2,16 @@ package ARP;
 
 public class RoutingTable {
 	
-	private byte[] RT_dstAddress;
-	private byte[] RT_subnetMask;
-	private String RT_gateway;
-	private boolean RT_flag_Up;
-	private boolean RT_flag_Gateway;
-	private int RT_Interface;
+	private byte[] RT_dstAddr;		// Destination address
+	private byte[] RT_subnetMask;		// Subnet Mask
+	private String RT_gateway;			// Gateway, 연결(*) 표시를 위해 String
+	private boolean RT_flag_Up;			// Flag U
+	private boolean RT_flag_Gateway;	// Flag UG
+	private int RT_Interface;			// Interface
 	
 	public RoutingTable() {
 		
-		RT_dstAddress = new byte[4];
+		RT_dstAddr = new byte[4];
 		RT_subnetMask = new byte[4];
 		RT_gateway = null;
 		RT_flag_Up = false;
@@ -19,16 +19,43 @@ public class RoutingTable {
 		RT_Interface = 0;
 	}
 	
-	// routing table에 add하는 함수
-	public void addRoutingEntry(byte[] dstAddress, byte[] subnetMask, String gateway, boolean flag_Up, boolean flag_Gateway, int Interface) {
+	// Routing table에 add하는 함수
+	public void addRoutingEntry(byte[] dstAddr, byte[] subnetMask, String gateway, boolean flag_Up, boolean flag_Gateway, int Interface) {
 
 		RoutingTable rt = null;
 		
-		rt.RT_dstAddress = dstAddress;
+		rt.RT_dstAddr = dstAddr;
 		rt.RT_subnetMask = subnetMask;
 		rt.RT_gateway = gateway;
 		rt.RT_flag_Up = flag_Up;
 		rt.RT_flag_Gateway = flag_Gateway;
 		rt.RT_Interface = Interface;
+		
 	}
+	
+	// 아래는 Get 함수들
+	public byte[] getDstAddr() {
+		return this.RT_dstAddr;
+	}
+	
+	public byte[] getSubnetMask() {
+		return this.RT_subnetMask;
+	}
+	
+	public String getGateway() {
+		return this.RT_gateway;
+	}
+	
+	public boolean getFlagUp() {
+		return this.RT_flag_Up;
+	}
+	
+	public boolean getFlagGateway() {
+		return this.RT_flag_Gateway;
+	}
+	
+	public int getInterface() {
+		return this.RT_Interface;
+	}
+	
 }
