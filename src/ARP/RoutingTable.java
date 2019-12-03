ec6b75d492cc98719524c9fa1241a78f75e4fec0
pjw9412 <pjwts9412@gmail.com>
2019-12-03 12:26:23 +0900

    충돌 병합

--- src/ARP/RoutingTable.java
+++ src/ARP/RoutingTable.java
@@ -18,36 +18,6 @@ public class RoutingTable {
 	public Map<String, _Routing_Entry> routing_Table = new LinkedHashMap<String, _Routing_Entry>();
 	public Set<String> routing_Table_Itr = routing_Table.keySet();
 	
-	private byte[] RT_dstAddr;			// Destination address
-	private byte[] RT_subnetMask;		// Subnet Mask
-	private String RT_gateway;			// Gateway, 연결(*) 표시를 위해 String
-	private boolean RT_flag_Up;			// Flag U
-	private boolean RT_flag_Gateway;	// Flag UG
-	private int RT_Interface;			// Interface
-//	private int RT_Class;				// Subnet 클래스 구분, 필요하면 추가
-	
-	public RoutingTable() {
-		  
-		RT_dstAddr = new byte[4];
-		RT_subnetMask = new byte[4];
-		RT_gateway = null;
-		RT_flag_Up = false;
-		RT_flag_Gateway = false;
-		RT_Interface = 0;
-	}
-	
-	// Routing table에 add하는 함수
-	public void addRoutingEntry(byte[] dstAddr, byte[] subnetMask, String gateway, boolean flag_Up, boolean flag_Gateway, int Interface) {
-
-		RT_dstAddr = dstAddr;
-		RT_subnetMask = subnetMask;
-		RT_gateway = gateway;
-		RT_flag_Up = flag_Up;
-		RT_flag_Gateway = flag_Gateway;
-		RT_Interface = Interface;
-		
-	}
-	
 	// router additional implementation
 	public class _Routing_Entry implements Comparable<_Routing_Entry> {
 		private _IP_ADDR subnetMask;
